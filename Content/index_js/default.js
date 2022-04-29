c2e12260b9226a48e885d5fefda31aabd447184e
ChiAn <ChiAn_Ate@Outlook.com>
2022-04-29 11:47:36 +0800

    函桉更改了文件。

--- Content/index_js/default.js
+++ Content/index_js/default.js
@@ -1,13 +1,7 @@
 
 var aaa=document.documentElement;
-var bbb=document.getElementById('a');
 aaa.addEventListener("mousemove",(e)=>{
     var {clientX,clientY}=e;
     aaa.style.setProperty('--x',clientX-40+'px');
     aaa.style.setProperty('--y',clientY-40+'px');
 });
-
-if(aaa.onmousemove){
-    alert('aa');
-
-}
