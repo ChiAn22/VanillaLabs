ac8fa4069a70fe2c7bcca9c10764a197161eb745
ChiAn <ChiAn_Ate@Outlook.com>
2022-05-03 18:40:36 +0800

    函桉更改了文件。

--- Resource/Alpha/Package/VanillaLabs/Content_Package/Packet/001Avocado/page/page_/page.js
+++ Resource/Alpha/Package/VanillaLabs/Content_Package/Packet/001Avocado/page/page_/page.js
@@ -1,3 +1,24 @@
+function PageInitialize(){
+    var Object=document.createElement('div');
+
+    Object.className='content_rightInner_Content';
+    Object.innerHTML='<div id="content_rightInner_left">\n' +
+        '                <div class="content_rightInner_leftTitle" style="background: #000000">\n' +
+        '                    <p class="content_rightInner_leftTitle_text">Content name</p>\n' +
+        '                </div>\n' +
+        '                <div class="content_rightInner_leftTitle" style="background: #910d0d">\n' +
+        '                    <p class="content_rightInner_leftTitle_text">70%折扣中</p>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '            <div id="content_rightInner_right"></div>';
+
+
+
+    var ContentForPage=document.getElementById('content_rightInner');
+    ContentForPage.appendChild(Object);
+}
+PageInitialize();
+
 
 function TouchedCircle(){
     var Circle_1=document.getElementById('a');
