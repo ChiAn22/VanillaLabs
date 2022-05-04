1d9680d65d14361c5f013b775b1007ca3f441f9a
ChiAn <ChiAn_Ate@Outlook.com>
2022-05-04 14:03:14 +0800

    函桉更改了文件。

--- Resource/Alpha/Package/VanillaLabs/Content_Package/Packet/001Avocado/page/page_/Model/model.js
+++ Resource/Alpha/Package/VanillaLabs/Content_Package/Packet/001Avocado/page/page_/Model/model.js
@@ -5,7 +5,8 @@ CHeight=Content_rightInner_right.offsetHeight;
 CWidth=Content_rightInner_right.offsetWidth;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75,  CWidth / CHeight , 0.1, 5000 );
+
+const camera = new THREE.PerspectiveCamera( 50,  CWidth / CHeight , 0.1, 5000 );
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 
 renderer.setSize( CWidth, CHeight );
@@ -19,14 +20,17 @@ loader.load('page_/Model/avocado.gltf', function(gltf) {
     gltf.scene.rotation.y=2.8;
 })
 
-var light1 = new THREE.AmbientLight(0xffffff,0.8);
-var light2 = new THREE.PointLight(0xffffff,0.2);
-//var light3 = new THREE.DirectionalLight(0xffffff,0.5);
+var light1 = new THREE.AmbientLight(0xffffff,0.4);
+var light2 = new THREE.PointLight(0xffffff,0.6,0,5);
+var light3 = new THREE.PointLight(0xffffff,1,0,0.2);
 
 scene.add(light1);
 scene.add(light2);
-//scene.add(light3);
-scene.background=new THREE.Color(0x565656);
+scene.add(light3);
+light3.position.y=3;
+light3.position.x=2;
+light3.position.z=-2;
+scene.background=new THREE.Color(0x141414);
 
 camera.position.x = 4;
 
