940f8a1acc79f0988cbab293469e8a224908b7ad
ChiAn <ChiAn_Ate@Outlook.com>
2022-05-09 15:50:40 +0800

    函桉更改了文件。

--- Content/user/user.js
+++ Content/user/user.js
@@ -1,16 +1,1 @@
-function TouchedCircle(){
-    var Circle_1=document.getElementById('a');
-    Circle_1.style.setProperty('visibility','visible');
-    Circle_1.style.setProperty('opacity','1');
-}
-function TouchedCircleHidden(){
-    var Circle_1=document.getElementById('a');
-    Circle_1.style.setProperty('visibility','hidden');
-    Circle_1.style.setProperty('opacity','0');
-}
-var aaa=document.documentElement;
-aaa.addEventListener("mousemove",(e)=>{
-    var {clientX,clientY}=e;
-    aaa.style.setProperty('--x',clientX-100+'px');
-    aaa.style.setProperty('--y',clientY-100+'px');
-});
+
