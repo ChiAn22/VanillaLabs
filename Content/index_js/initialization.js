616616e2f8f84e91f01ec8b87ddfe06419cf9c9e
ChiAn <ChiAn_Ate@Outlook.com>
2022-05-10 18:30:07 +0800

    函桉更改了文件。

---
+++ Content/index_js/initialization.js
@@ -0,0 +1,35 @@
+    let element = document.createElement('div');
+    element.className='content_leftInner_list_bottom_Inner';
+    let _contentText1,_contentText2,_contentText3,num,_EmailText,_NameText;
+
+    if("string" !== typeof window.localStorage.getItem('User_Statement')){
+        _contentText1='登录';
+        _contentText2='';
+        _contentText3='';
+    }else if(window.localStorage.getItem('User_Statement').split('#,')[1]==='1'){
+        num=parseInt(window.localStorage.getItem('User_Statement').split('#,')[0]);
+        _EmailText=window.localStorage.getItem('User_Archive'+num).split('#,')[5];
+        _NameText=window.localStorage.getItem('User_Archive'+num).split('#,')[1];
+        _contentText1=_NameText.substr(0,2);
+        _contentText2=_NameText;
+        _contentText3=_EmailText;
+    }else {
+        _contentText1='登录';
+        _contentText2='';
+        _contentText3='';
+    }
+
+
+    element.innerHTML='<div class="content_leftInner_list_bottom_Inner_list" style="width: 20%">\n' +
+        '                        <a href="./user/login.html" style="text-decoration: none;height: 50px;width:50px;border-radius: 25px;background: #000000;display: flex;align-items: center;justify-content: center">\n' +
+        '                            <div style="color: #FFFFFF;font-weight: bold">'+_contentText1+'</div>\n' +
+        '                        </a>\n' +
+        '                    </div>\n' +
+        '                    <div class="content_leftInner_list_bottom_Inner_list" style="width: 80%">\n' +
+        '                        <div style="margin-left: 10px;height: 100%;width: 100%;display: flex;justify-content: center;flex-direction: column;overflow: hidden;">\n' +
+        '                            <p style="font-size: 14px;color: #FFFFFF;font-weight: bold;text-align: left">'+_contentText2+'</p>\n' +
+        '                            <p style="font-size: 14px;color: #FFFFFF;font-weight: bold;text-align: left">'+_contentText3+'</p>\n' +
+        '                        </div>\n' +
+        '                    </div>';
+
+    document.getElementById('content_leftInner_list_bottom').appendChild(element);
